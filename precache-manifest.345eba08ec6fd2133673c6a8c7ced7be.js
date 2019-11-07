4fbf130f7111b56030841e317d2b27d2613bde96
Dextrous <ssjdexucyche@gmail.com>
2019-11-07 16:44:51 +0530

    Updates

---
+++ precache-manifest.345eba08ec6fd2133673c6a8c7ced7be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db2c1640308c20794acceb69671f3e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c43eb453b9670d6d51c2",
+    "url": "/static/css/main.7c3d1ac0.chunk.css"
+  },
+  {
+    "revision": "e101d6bd52532323e9a0",
+    "url": "/static/js/2.07f13d8b.chunk.js"
+  },
+  {
+    "revision": "c43eb453b9670d6d51c2",
+    "url": "/static/js/main.6f5006df.chunk.js"
+  },
+  {
+    "revision": "67d79d8a1e987f775a6b",
+    "url": "/static/js/runtime-main.5a65090d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
